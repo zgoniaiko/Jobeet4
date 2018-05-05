cfb6fed82932c8bcc0d079b3911deb9ae7530209
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2018-05-05 03:42:33 +0300

    Add fixtures for jobs and categories

---
+++ src/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
+
+class LoadFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $loader = new NativeLoader();
+        $objectSet = $loader->loadFiles([
+            __DIR__.'/categories.yaml',
+            __DIR__.'/jobs.yaml',
+        ])->getObjects();
+
+        foreach($objectSet as $object) {
+            $manager->persist($object);
+        }
+
+        $manager->flush();
+    }
+
+    public function getId(): string
+    {
+        
+    }
+
+}
