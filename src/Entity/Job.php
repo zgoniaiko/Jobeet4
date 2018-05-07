aea738801b2f08b3c7d4c7bebe1141ff164170c6
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2018-05-07 20:48:40 +0300

    Set expiry date for job on prePersist

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -303,6 +303,10 @@ class Job
     {
         $this->createdAt = new \DateTime();
         $this->updatedAt = new \DateTime();
+
+        if (!$this->expiresAt) {
+            $this->expiresAt = (clone $this->createdAt)->modify('+30 days');
+        }
     }
 
     /**
