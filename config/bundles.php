12800faabde1564ce55f82453009376cba4ce3aa
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2018-05-08 00:06:36 +0300

    Add doctrine extensions package

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
